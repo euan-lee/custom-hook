6efa4702c5a5fbc0a430c3b029a2a2b043b62150
euan <maplestory1419@gmail.com>
2021-07-07 17:33:22 +0900

    [useinput]

---
+++ src/useinput.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+const useInput=(initialValue,validator)=>{
+const [value,setValue]=useState(initialValue);
+
+const onChange = event => {
+  const {
+    target: { value }
+  }=event;
+  let willUpdate=true;
+  if(typeof validator==="function"){
+    willUpdate=validator(value);
+  }
+  if(willUpdate){
+      setValue(value);
+  };
+};
+
+return {value,onChange};
+}
+
+
+function App() {
+  const maxLen = value =>value.length < 10;
+ const name=useInput("Mr.",maxLen);
+
+  return (
+    <div>
+    <h1>hello</h1>
+    <input placeholder="Name"  {...name}/>
+    </div>
+  );
+}
+
+
+export default App;
