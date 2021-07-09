4f8ef7ca026346528b87c1eb36af7e561247bf05
euan <maplestory1419@gmail.com>
2021-07-09 20:14:05 +0900

    [click]

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React,{useState,useEffect,useRef} from 'react'
+
+
+const useClick=(onClick)=>{
+  const element=useRef();
+  useEffect(()=>{
+    if(element.current){
+      element.current.addEventListener("click",onClick)
+    }
+    return ()=>{
+      if(element.current){
+        element.current.removeEventListener("click",onClick)
+      }
+     
+    }
+  },[]);
+  return element;
+}
+
+function App() {
+  const sayHello=()=>{
+    console.log("sayHello");
+  }
+const title=useClick(sayHello);
+
+  return (
+    <div className="App">
+      <h1 ref={title}> hi</h1>
+    </div>
+  )
+}
+
+export default App
